f66b8a2b8ba2c0e6fbeb2e6f868bacbf5e2f7ac1
sofi04080 <sofi04080@gmail.com>
2026-09-09 19:12:52 +0900

    [Passed] 연속되는 수 3 (230ms, 14MB)

---
+++ CodeTree/trail2/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // Please write your code here.
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            int ans = 1;
+            if(i == 0 || (arr[i] < 0 && arr[i-1] < 0)){
+                int j = i;
+                while(j < n && j != 0 && (arr[j] < 0 && arr[j-1] < 0)) {
+                    j++;
+                    ans++;   
+                }
+            } else if (i == 0 || (arr[i] > 0 && arr[i-1] > 0)){
+                int j = i;
+                while(j < n && j != 0 && (arr[j] > 0 && arr[j-1] > 0 )){
+                    j++;
+                    ans++;    
+                }
+            }
+            if( ans > max)
+            max = ans;
+        }   
+        
+        System.out.print(max);
+    }
+}
