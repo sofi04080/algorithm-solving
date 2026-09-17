73811e27fdca8808f9a988390ee41959125c4fe5
sofi04080 <sofi04080@gmail.com>
2026-09-17 13:45:13 +0900

    [Passed] 모이자 (142ms, 10MB)

---
+++ CodeTree/trail2/모이자/gather.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        // Please write your code here.
+    
+        int ans = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            int sum = 0; 
+            for(int j = 0; j < n; j++) {
+                sum += Math.abs(i-j) * a[j];
+            }
+
+            if(sum < ans)
+                ans = sum;
+        }
+
+
+        System.out.println(ans);
+    }
+}
